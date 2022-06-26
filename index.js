ea3ea637bc0917f6aae14009f275448d29fffc2c
Kevin McIntosh <kmac303@gmail.com>
2022-06-25 19:15:12 -0600

    commit tree 765df92a1719e9670178b7435ede16d6d7fa8016

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+function superbowlWin(arr) {
+   let win = arr.find(element => element.result === 'W')
+//    if (win === undefined) {
+//     return win;
+//    } else {
+//     return win.year;
+//    }
+   return win === undefined ? win : win.year
+}
